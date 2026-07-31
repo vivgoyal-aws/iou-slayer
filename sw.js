fa430c6a70812cd305d14dc10cedd7d38ecbf941
Vivek Goyal <vivek-goyal@hotmail.com>
2026-07-31 13:59:30 -0700

    fix: insallation

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_NAME = 'iou-slayer-v1';
-const ASSETS = ['/index.html', '/manifest.json'];
+const ASSETS = ['./index.html', './manifest.json'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
@@ -15,6 +15,6 @@ self.addEventListener('activate', e => {
 
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match('/index.html')))
+    caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match(new URL('./index.html', self.location.href).href)))
   );
 });
